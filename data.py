4f8c89ecf848a831048d68aa59c7e119e13f3587
Wang.Y <294961525@qq.com>
2026-05-10 11:00:04 +0800

    260510: Finished load_new_category

--- data.py
+++ data.py
@@ -1,15 +1,20 @@
+import os
 import json
+import torch
 import multiprocessing
 from pathlib import Path
 from collections import Counter
+from transformers import BertTokenizer
 from torch.utils.data import Dataset
 
+__all__ = ['load_news_category']
 
-class NewsCategoryDataset(Dataset):
-    def __init__(self, fpath, tokenizer):
+
+class _NewsCategoryDataset(Dataset):
+    def __init__(self, fpath: str, tokenizer: BertTokenizer):
         self.fpath = Path(fpath)
         self.tokenizer = tokenizer
-        self.max_len = 160
+        self.max_len = 40
         self.all_token_ids, self.all_segments, self.all_attention_mask, self.labels = self._init_dataset()
 
     def __getitem__(self, idx):
@@ -39,7 +44,7 @@ class NewsCategoryDataset(Dataset):
         # load News_Category_Dataset_v3.json
         features, labels = [], []  # short_description, category
         with open(self.fpath, 'r', encoding='utf-8') as file:
-            for idx, line in enumerate(file):
+            for line in file:
                 line = json.loads(line)
                 short_description = self._format(line["short_description"])
                 if len(short_description) == 0:
@@ -79,3 +84,64 @@ class NewsCategoryDataset(Dataset):
     def _mp_worker(self, feature):
         out = self.tokenizer(feature, truncation=True, padding='max_length', max_length=self.max_len)
         return out["input_ids"], out["token_type_ids"], out["attention_mask"]
+
+
+def _split_new_category_dataset(fpath: Path, fname: str):
+    # Count the original labels
+    labels = []
+    with open(fpath, 'r', encoding='utf-8') as file:
+        for idx, line in enumerate(file):
+            line = json.loads(line)
+            if len(line["short_description"]) == 0:
+                continue
+            labels.append(line["category"])
+    labels_count = dict(Counter(labels))
+
+    # split dataset
+    train, test = [], []
+    vel_count = {key: int(labels_count[key] * 0.1) for key in labels_count}
+    _vel_count = {key: 0 for key in vel_count}
+    with open(fpath, 'r', encoding='utf-8') as file:
+        for line in file:
+            label = json.loads(line)["category"]
+            if _vel_count[label] <= vel_count[label]:
+                test.append(line)
+                _vel_count[label] += 1
+            else:
+                train.append(line)
+
+    # save
+    with open(fpath.parent / f"{fname}_train.json", "w", encoding="utf-8") as f:
+        f.write("".join(train))
+    with open(fpath.parent / f"{fname}_test.json", "w", encoding="utf-8") as f:
+        f.write("".join(test))
+
+
+def load_news_category(fpath: str, local_model_path: str, batch_size: int):
+    """
+    ARGS:
+        - fpath: path to News_Category_Dataset_v3.json
+    """
+    fpath = Path(fpath)
+    fname = fpath.stem
+    if fpath.suffix != ".json":
+        raise ValueError("We only accept JSON files.")
+    split_dataset = True
+    num_workers = os.cpu_count() // 2
+
+    for item in fpath.parent.iterdir():
+        if item.name == f"{fname}_train.json" or item.name == f"{fname}_test.json":
+            split_dataset = False
+            break
+    if split_dataset:
+        _split_new_category_dataset(fpath, fname)
+        print(f"Split {fname}_train.json and {fname}_text.json from {fname}")
+
+    tokenizer = BertTokenizer.from_pretrained(local_model_path)
+
+    train_set = _NewsCategoryDataset(fpath.parent / f"{fname}_train.json", tokenizer)
+    train_iter = torch.utils.data.DataLoader(train_set, batch_size, shuffle=True, num_workers=num_workers)
+    test_set = _NewsCategoryDataset(fpath.parent / f"{fname}_test.json", tokenizer)
+    test_iter = torch.utils.data.DataLoader(test_set, batch_size, shuffle=False, num_workers=num_workers)
+
+    return train_iter, test_iter
